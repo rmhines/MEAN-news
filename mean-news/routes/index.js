a95f7699734c856eadc543f7df8556cb5dc585f4
Bobby Hines <bobby.m.hines@gmail.com>
2016-01-10 13:36:39 -0800

    Set up some initial routing for persistent storage

--- mean-news/routes/index.js
+++ mean-news/routes/index.js
@@ -7,3 +7,37 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+// "GET" route for retrieving posts
+// req - Contains all request info made to the server
+// res - Object used to respond to the client
+router.get('/posts', function(req, res, next) {
+	// Query the database for all posts
+	Post.find(function(err, posts) {
+		if (err) {
+			return next(err);
+		}
+
+		// If no errors, send the retrieved posts back to the client
+		res.json(posts);
+	});
+});
+
+// "POST" route for creating posts
+router.post('/posts', function(req, res, next) {
+	// Use mongoose to create a new post object
+	var post = new Post(req.body);
+
+	// If no errors, save the post to the database
+	post.save(function(err, post) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(post);
+	});
+});
