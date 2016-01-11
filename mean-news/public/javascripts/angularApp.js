6576ae7c73816188ef91a1192cb680a2331bb860
Bobby Hines <bobby.m.hines@gmail.com>
2016-01-10 19:03:47 -0800

    Finish wiring the front and back ends together

--- mean-news/public/javascripts/angularApp.js
+++ mean-news/public/javascripts/angularApp.js
@@ -7,79 +7,171 @@ app.config([
         $stateProvider.state('home', {
             url: '/home',
             templateUrl: '/home.html',
-            controller: 'MainCtrl'
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: [
+                    'posts',
+                    function(posts) {
+                        return posts.getAll();
+                    }
+                ]
+            }
         }).state('posts', {
             url: '/posts/{id}',
             templateUrl: '/posts.html',
-            controller: 'PostsCtrl'
+            controller: 'PostsCtrl',
+            resolve: {
+                post: [
+                    '$stateParams',
+                    'posts',
+                    function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }
+                ]
+            }
         });
         $urlRouterProvider.otherwise('home');
     }
 ]);
 
-app.controller('MainCtrl', [
-    '$scope',
-    'posts',
-    function($scope, posts) {
-        $scope.posts = posts.posts;
+app.controller(
+    'MainCtrl', [
+        '$scope',
+        'posts',
+        function($scope, posts) {
+            $scope.posts = posts.posts;
 
-        $scope.addPost = function() {
-            // Prevent users from adding posts with blank titles
-            if (!$scope.title || $scope.title === '') {
-                return;
+            $scope.addPost = function() {
+                // Prevent users from adding posts with blank titles
+                if (!$scope.title || $scope.title === '') {
+                    return;
+                }
+                // Save posts to the server
+                posts.create({
+                    title: $scope.title,
+                    link: $scope.link
+                });
+
+                // Reset input field to empty strings when complete
+                $scope.title = '';
+                $scope.link = '';
+            };
+
+            $scope.incrementUpvotes = function(post) {
+                posts.upvote(post);
+            };
+
+            $scope.decrementUpvotes = function(post) {
+                posts.downvote(post);
+            };
+        }
+    ]
+);
+
+app.controller(
+    'PostsCtrl', [
+        '$scope',
+        'posts',
+        'post',
+        function($scope, posts, post) {
+            $scope.post = post;
+
+            $scope.addComment = function () {
+                if ($scope.body === '') {
+                    return;
+                }
+
+                posts.addComment(post._id, {
+                    body: $scope.body,
+                    author: 'user'
+                }).success(function(comment) {
+                    $scope.post.comments.push(comment);
+                });
+
+                $scope.body = '';
+            };
+
+            $scope.incrementUpvotes = function(comment) {
+                posts.upvoteComment(post, comment);
             }
-            $scope.posts.push({
-                title: $scope.title,
-                link: $scope.link,
-                upvotes: 0,
-                comments: [
-                    {author: 'Joe', body: 'Cool post!', upvotes: 3},
-                    {author: 'Bob', body: 'Great idea, but...', upvotes: -1},
-                ]
-            });
 
-            // Reset input field to empty strings
-            $scope.title = '';
-            $scope.link = '';
-        }
-
-        $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
-        }
-
-        $scope.decrementUpvotes = function(post) {
-            post.upvotes -= 1;
-        }
-    }
-]);
-
-app.controller('PostsCtrl', [
-    '$scope',
-    '$stateParams',
-    'posts',
-    function($scope, $stateParams, posts) {
-        $scope.post = posts.posts[$stateParams.id];
-
-        $scope.addComment = function () {
-            if ($scope.body === '') {
-                return;
+            $scope.decrementUpvotes = function(comment) {
+                posts.downvoteComment(post, comment);
             }
-            $scope.post.comments.push({
-                body: $scope.body,
-                author: 'user',
-                upvotes: 0
-            });
-            $scope.body = '';
-        };
-    }
-]);
+        }
+    ]
+);
 
 // Factory service to make posts available across views
-app.factory("posts", [
-    function() {
-        var service = {
-            posts: []
-        };
-        return service;
-    }
-]);
+// Uses the Angular $http service to query the posts route
+app.factory(
+    "posts", [
+        '$http',
+        function($http) {
+            var service = {
+                posts: []
+            };
+            
+            // Retrieve all posts
+            service.getAll = function() {
+                // Use success() to define what executes when request returns
+                return $http.get('/posts').success(function(data) {
+                    angular.copy(data, service.posts);
+                });
+            };
+
+            // Create new post
+            service.create = function(post) {
+                return $http.post('/posts', post).success(function(data) {
+                    service.posts.push(data);
+                });
+            };
+
+            // Upvote posts
+            service.upvote = function(post) {
+                return $http.put('/posts/' + post._id + '/upvote')
+                .success(function(data) {
+                    post.upvotes += 1;
+                });
+            };
+
+            // Downvote posts
+            service.downvote = function(post) {
+                return $http.put('/posts/' + post._id + '/downvote')
+                .success(function(data) {
+                    post.upvotes -= 1;
+                });
+            };
+
+            // Retrieve a single post
+            service.get = function(id) {
+                return $http.get('/posts/' + id).then(function(res) {
+                    return res.data
+                });
+            };
+
+            // Add comment to a post
+            service.addComment = function(id, comment) {
+                return $http.post('/posts/' + id + '/comments', comment);
+            };
+
+            // Upvote a comment
+            service.upvoteComment = function(post, comment) {
+                return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+                .success(function(data) {
+                    comment.upvotes += 1;
+                });
+            };
+
+            // Downvote a comment
+            service.downvoteComment = function(post, comment) {
+                return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/downvote')
+                .success(function(data) {
+                    comment.upvotes -= 1;
+                });
+            };
+
+            return service;
+        }
+    ]
+);
