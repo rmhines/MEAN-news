f3c1d8b59647ad26010347bce0fcb42107a317b4
Bobby Hines <bobby.m.hines@gmail.com>
2016-01-10 17:55:20 -0800

    Add backend routing for comment upvotes and downvotes

--- mean-news/models/Comments.js
+++ mean-news/models/Comments.js
@@ -3,8 +3,24 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
 	body: String,
 	author: String,
-	upvotes: { type: Number, default: 0 },
-	post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+	upvotes: { 
+		type: Number, 
+		default: 0 
+	},
+	post: { 
+		type: mongoose.Schema.Types.ObjectId, 
+		ref: 'Post' 
+	}
 });
+
+CommentSchema.methods.upvote = function(commentParam) {
+	this.upvotes += 1;
+	this.save(commentParam);
+};
+
+CommentSchema.methods.downvote = function(commentParam) {
+	this.upvotes -= 1;
+	this.save(commentParam);
+};
 
 mongoose.model('Comment', CommentSchema);
--- mean-news/routes/index.js
+++ mean-news/routes/index.js
@@ -60,12 +60,35 @@ router.param('post', function(req, res, next, id) {
 	});
 });
 
-// Route for returning a single post
-router.get('/posts/:post', function(req, res) {
-	res.json(req.post);
+// Route for preloading comment object
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function (err, comment) {
+		if (err) {
+			return next(err);
+		}
+		if (!comment) {
+			return next(new Error('Unable to locate comment.'));
+		}
+
+		req.comment = comment;
+		return next();
+	});
 });
 
-// Route for upvotes
+// Route for returning a single post (and its comments with .populate)
+router.get('/posts/:post', function(req, res, next) {
+	req.post.populate('comments', function(err, post) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(req.post);
+	});
+});
+
+// Route for post upvotes
 router.put('/posts/:post/upvote', function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if (err) {
@@ -76,7 +99,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	});
 });
 
-// Route for downvotes
+// Route for post downvotes
 router.put('/posts/:post/downvote', function(req, res, next) {
 	req.post.downvote(function(err, post) {
 		if (err) {
@@ -84,5 +107,47 @@ router.put('/posts/:post/downvote', function(req, res, next) {
 		}
 
 		res.json(post);
+	});
+});
+
+// Comments route for a particular post
+router.post('/posts/:post/comments', function(req, res, next) {
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment) {
+		if (err) {
+			return next(err);
+		}
+
+		req.post.comments.push(comment);
+		req.post.save(function(err, post) {
+			if (err) {
+				return next(err);
+			}
+			res.json(comment);
+		});
+	});
+});
+
+// Route for comment upvotes
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.comment.upvote(function(err, comment) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(comment);
+	});
+});
+
+// Route for comment downvotes
+router.put('/posts/:post/comments/:comment/downvote', function(req, res, next) {
+	req.comment.downvote(function(err, comment) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(comment);
 	});
 });
